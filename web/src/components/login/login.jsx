0da7ccddef834e77cdf1facb84c9c98985836ac0
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-02-21 22:10:29 +0500

    work done

--- web/src/components/login/login.jsx
+++ web/src/components/login/login.jsx
@@ -1,43 +1,67 @@
-import React from 'react';
-import './login.css';
-import axios from 'axios';
+import React from "react";
+import "./login.css";
+import axios from "axios";
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const [email , setEmail] = useState("");
-  const [password , setPassword] = useState("");
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log("Form Submitted")
+    console.log("Form Submitted");
 
-    axios.post('http://localhost:3000/login', {
-      email : email,
-      password: password
-    })
-    .then(function (response) {
-      console.log(response.data.message);
-    })
-    .catch(function (error) {
-      console.log(error.data.message);
-    });
-}
+    axios
+      .post("http://localhost:3000/login", {
+        email: email,
+        password: password,
+      })
+      .then(function (response) {
+        console.log(response.data.message);
+      })
+      .catch(function (error) {
+        console.log(error.data.message);
+      });
+  };
 
   return (
     <div className="login-form">
-     <div className='form'>
-       <form onSubmit={submitHandler}>
-        <h1>LogIn</h1>
-        <input type="email" placeholder="Email"  className="input-box" required onChange={(e)=>{setEmail(e.target.value)}} />
-        <input type="password" placeholder="Password"  className="input-box" required onChange={(e)=>{setPassword(e.target.value)}} />
-        <p>Forgot password?</p>
-        <input type="submit" value="Login" id="login-btn" />
-        <p> Don't have an account?   <Link to='/' id='link'> Sign Up </Link></p>
-       </form>
+      <div className="form">
+        <form onSubmit={submitHandler}>
+          <h1>LogIn</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            className="input-box"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="input-box"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <p>Forgot password?</p>
+          <input type="submit" value="Login" id="login-btn" />
+          <p>
+            {" "}
+            Don't have an account?{" "}
+            <Link to="/" id="link">
+              {" "}
+              Sign Up{" "}
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Login;
